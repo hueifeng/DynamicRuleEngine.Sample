b5f4a9201d6e3106155ea70540018f0bd9b42486
hueifeng <695979933@qq.com>
2025-03-23 22:21:57 +0800

    添加项目文件。

---
+++ DynamicRuleEngine.Sample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DynamicRuleEngine.Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
